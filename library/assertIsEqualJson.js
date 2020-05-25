22214588ee1a88a4c2fd7cb621d05621452415f9
JaredMathis <jaredmathis@protonmail.com>
2020-05-25 17:15:49 -0400

    update

--- library/assertIsEqualJson.js
+++ library/assertIsEqualJson.js
@@ -2,12 +2,15 @@ const assert = require("./assert");
 const scope = require('./scope');
 const isDefined = require("../core").isDefined;
 const isFunction = require("../core").isFunction;
+const merge = require("../log").merge;
 
 module.exports = assertIsEqualJson;
 
 function assertIsEqualJson(left, right) {
     let result;
     scope(assertIsEqualJson.name, x => {
+        merge(x, {left});
+        merge(x, {right});
         assert(() => isDefined(left));
         assert(() => isDefined(right));
 
@@ -17,12 +20,15 @@ function assertIsEqualJson(left, right) {
         } else {
             leftValue = left;
         }
+        merge(x, {leftValue});
+
         let rightValue;
         if (isFunction(right)) {
             rightValue = right();
         } else {
             rightValue = right;
         }
+        merge(x, {rightValue});
 
         assert(() => JSON.stringify(leftValue) === JSON.stringify(rightValue));
     });
