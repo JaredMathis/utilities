7f15de19c23b3c42007b320fd6c6149d1a3c3236
JaredMathis <jaredmathis@protonmail.com>
2020-05-27 16:12:08 -0400

    added tests

--- tests/propertiesAreEqual/propertiesAreEqual.js
+++ tests/propertiesAreEqual/propertiesAreEqual.js
@@ -4,5 +4,14 @@ const u = require("../../index");
 const propertiesAreEqual = require("../../library/propertiesAreEqual.js");
 
 u.scope(__filename, x => {
-    // TODO
+    u.assert(() => propertiesAreEqual({a:1}, {a:1}, ['a']));
+    u.assert(() => propertiesAreEqual({a:1,b:2}, {b:2,a:1}, ['a', 'b']));
+    u.assert(() => !propertiesAreEqual({a:1,b:2}, {b:2,a:2}, ['a', 'b']));
+    u.assert(() => propertiesAreEqual({a:1,b:2}, {b:2,a:1,c:3}, ['a', 'b']));
+    u.assert(() => !propertiesAreEqual({a:1,b:2}, {b:3,a:1,c:3}, ['a', 'b']));
+    u.assert(() => !propertiesAreEqual({a:1,b:2}, {b:2,a:2,c:3}, ['a', 'b']));
+    u.assert(() => propertiesAreEqual({a:1,b:2}, {b:2,a:1}, ['a', 'b', 'c']));
+    u.assert(() => propertiesAreEqual({a:1,b:2}, {b:2,a:1}, ['a']));
+    u.assert(() => !propertiesAreEqual({a:3,b:2}, {b:2,a:1}, ['a']));
+
 });
