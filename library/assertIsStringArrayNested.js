8734fc8df2d44a1ab9d49743b5ea6e77ecb3148c
JaredMathis <jaredmathis@protonmail.com>
2020-05-30 17:53:12 -0400

    fixed bug

--- library/assertIsStringArrayNested.js
+++ library/assertIsStringArrayNested.js
@@ -4,16 +4,20 @@ const assert = require("./assert");
 const loop = require("./loop");
 const isArray = require("./isArray");
 const assertIsStringArray = require("./assertIsStringArray");
+const merge = require("./merge");
+const unwrapIfLambda = require("./unwrapIfLambda");
 
 module.exports = assertIsStringArrayNested;
 
 function assertIsStringArrayNested(input) {
     let result;
     scope(assertIsStringArrayNested.name, x => {
-        assert(() => isArray(input));
+        merge(x,{input});
+        let value = unwrapIfLambda(input);
+        assert(() => isArray(value));
         
-        loop(input, l => {
-            assertIsStringArray(() => l);
+        loop(value, v => {
+            assertIsStringArray(() => v);
         });
     });
     return result;
--- tests/assertIsStringArrayNested/assertIsStringArrayNested.js
+++ tests/assertIsStringArrayNested/assertIsStringArrayNested.js
@@ -5,10 +5,17 @@ const assertIsStringArrayNested = require("../../library/assertIsStringArrayNest
 
 u.scope(__filename, x => {
     assertIsStringArrayNested([[]]);
+    assertIsStringArrayNested(() => [[]]);
     assertIsStringArrayNested([['a']]);
+    assertIsStringArrayNested(() => [['a']]);
     assertIsStringArrayNested([['a', 'b']]);
+    assertIsStringArrayNested(() => [['a', 'b']]);
     assertIsStringArrayNested([['a', 'b'],['c']]);
+    assertIsStringArrayNested(() => [['a', 'b'],['c']]);
     u.assertThrows(() => assertIsStringArrayNested([['a', 'b'],['c'], 'a']));
+    u.assertThrows(() => assertIsStringArrayNested(() => [['a', 'b'],['c'], 'a']));
     u.assertThrows(() => assertIsStringArrayNested([['a', 'b'],['c'], 1]));
+    u.assertThrows(() => assertIsStringArrayNested(() => [['a', 'b'],['c'], 1]));
     u.assertThrows(() => assertIsStringArrayNested(['a', 'v']));
+    u.assertThrows(() => assertIsStringArrayNested(() => ['a', 'v']));
 });
