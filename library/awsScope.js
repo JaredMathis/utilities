7c3bf3b20f05fbade934c8fdc189cbd893a20c23
JaredMathis <jaredmathis@protonmail.com>
2020-06-10 00:06:25 -0400

    update

--- library/awsScope.js
+++ library/awsScope.js
@@ -1,11 +1,15 @@
 
 const scope = require("./scope");
+const isFunction = require("./isFunction");
+const assert = require("./assert");
 
 module.exports = awsScope;
 
 function awsScope(name, lambda, callback) {
     try {
         scope(name, x => {
+            assert(() => isFunction(callback));
+
             let result = lambda(x);
             callback(null, JSON.stringify({
                 success: true,
